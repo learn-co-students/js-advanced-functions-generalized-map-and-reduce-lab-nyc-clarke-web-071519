37083e204866cabe0ea0fadd2cc61a77a3f1d297
Valentino Wong <valentino.wong@gmail.com>
2019-09-05 13:01:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+function map(src,transformerFunction) {
+    let newArray = [];
+    for (let i = 0; i < src.length; i++) {
+        newArray.push(transformerFunction(src[i]));
+    }
+    return newArray
+}
+
+function reduce(src,reducerFunction,startingValue) {
+    let memo
+    if (startingValue) {
+        memo = reducerFunction(src[0], startingValue);
+    } else {
+        memo = src[0]
+    }
+    for (let i = 1; i < src.length; i++) {
+        memo = reducerFunction(src[i], memo);
+    }
+
+    return memo
+}
